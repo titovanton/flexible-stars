9ea55a74a688785c76bb8a2ea146a3a9252fdf69
titovanton-com <mail@titovanton.com>
2013-03-11 04:56:36 +0600

    hover done

--- flexible-stars.js
+++ flexible-stars.js
@@ -1,72 +1,131 @@
 (function($){
-    $.fn.flexible_stars = function (init) {
-        var target = $(this)
-        var settings = {
-            "gold"   : "sprite-gold-star",
-            "silver" : "sprite-silver-star",
-            "half"   : "sprite-half-star",
-            "url"    : "/stars/handler/",
-            "init"   : "0"
-        }
+    $.fn.flexible_stars = function (js_init) {
 
-        $.extend(settings, init)
+        function init(target) {
+            // default init
+            var settings = {
+                "gold"   : "sprite-gold-star",
+                "silver" : "sprite-silver-star",
+                "half"   : "sprite-half-star",
+                "url"    : "/stars/handler/",
+                "init"   : "0"
+            }
 
-        // generating widget content
-        var fraction = Math.round((settings.init - Math.floor(settings.init))*100)/100
-
-        console.log(fraction)
-        if (settings.init * 1 === 0) {
-            console.log("if: 1")
-            for (var i = 0; i < 5; i++) target.append('<i class=' + settings.silver + '/>')
-        }
-        else if (fraction >= 0.25 && fraction <= 0.74) {
-            console.log("if: 2")
-            for (var i = 0; i < Math.floor(settings.init); i++)
-                target.append('<i class=' + settings.gold + '/>')
-            target.append('<i class=' + settings.half + '/>')
-            for (var i = Math.ceil(settings.init); i < 5; i++)
-                target.append('<i class=' + settings.silver + '/>')
-        }
-        else if (fraction < 0.25 && fraction >= 0){
-            console.log("if: 3")
-            for (var i = 0; i < Math.floor(settings.init); i++)
-                target.append('<i class=' + settings.gold + '/>')
-            for (var i = Math.floor(settings.init); i < 5; i++)
-                target.append('<i class=' + settings.silver + '/>')
-        }
-        else if (fraction < 1 && fraction > 0.74){
-            console.log("if: 4")
-            for (var i = 0; i < Math.ceil(settings.init); i++)
-                target.append('<i class=' + settings.gold + '/>')
-            for (var i = Math.ceil(settings.init); i < 5; i++)
-                target.append('<i class=' + settings.silver + '/>')
-        }
-
-        // mouse hover widget effects
-
-        return target
-    }
-
-    $(function(){
-        $('.flexible-stars').each(function(){
-            var target       = $(this)
-            var init         = {}
-            var for_template = [
-                "gold",
-                "silver",
-                "half",
-                "url",
-                "init"
-            ]
+            // HTML attr init
+            var for_template = []
+            for(var k in settings) for_template.push(k)
 
             for (var i = 0; i < for_template.length; i++) {
                 var tag = 'data-' + for_template[i]
-                if (typeof(target.attr(tag)) != "undefined" && target.attr(tag) != "undefined") {
-                    init[for_template[i]] = target.attr(tag)
+                if (typeof(target.attr(tag)) != "undefined") {
+                    settings[for_template[i]] = target.attr(tag)
                 }
             }
 
-            target.flexible_stars(init)
+            // init from js
+            $.extend(settings, js_init)
+
+            // stor data for future needs
+            target.data('flexible_stars', settings)
+        }
+
+        function clear_star(star) {
+            var settings = star.parent().data('flexible_stars')
+
+            star
+                .removeClass(settings.gold)
+                .removeClass(settings.silver)
+                .removeClass(settings.half)
+
+            return star
+        }
+
+        function drow_stars(target) {
+            var settings = target.data('flexible_stars')
+            var fraction = Math.round((settings.init - Math.floor(settings.init))*100)/100
+
+            if (settings.init * 1 === 0) {
+                target.find('i').each(function(){
+                    clear_star($(this))
+
+                    $(this).addClass(settings.silver)
+                })
+            }
+            else if (fraction >= 0.25 && fraction <= 0.74) {
+                var i = 0
+
+                target.find('i').each(function(){
+                    clear_star($(this))
+
+                    if (i < Math.floor(settings.init))       $(this).addClass(settings.gold)
+                    else if (i == Math.floor(settings.init)) $(this).addClass(settings.half)
+                    else                                     $(this).addClass(settings.silver)
+                    i++
+                })
+            }
+            else if (fraction < 0.25 && fraction >= 0){
+                var i = 0
+
+                target.find('i').each(function(){
+                    clear_star($(this))
+
+                    if (i < Math.floor(settings.init)) $(this).addClass(settings.gold)
+                    else                               $(this).addClass(settings.silver)
+                    i++
+                })
+            }
+            else if (fraction < 1 && fraction > 0.74){
+                var i = 0
+
+                target.find('i').each(function(){
+                    clear_star($(this))
+
+                    if (i < Math.ceil(settings.init)) $(this).addClass(settings.gold)
+                    else                              $(this).addClass(settings.silver)
+                    i++
+                })
+            }
+        }
+
+        function mouse_enter_handler(eventObject) {
+            var $this    = this
+            var parent   = $(this).parent()
+            var settings = parent.data('flexible_stars')
+
+            if (typeof(settings.set) == "undefined") {
+                var set_class = settings.gold
+
+                parent.find('i').each(function(){
+                    clear_star($(this))
+                        .addClass(set_class)
+
+                    if (this == $this) {
+                        set_class = settings.silver
+                    }
+                })
+            }
+        }
+
+        function mouse_leave_handler(eventObject) {
+            drow_stars($(this).parent())
+        }
+
+        return this.each(function() {
+            var target = $(this)
+            init(target)
+
+            // generate content
+            for (var i = 0; i < 5; i++) target.append('<i/>')
+
+            drow_stars(target)
+
+            target.find('i').hover(mouse_enter_handler, mouse_leave_handler)
         })
+
+    }
+
+    $(function(){
+        $('.flexible-stars').flexible_stars()
     })
-})(jQuery)
+
+})(jQuery);
